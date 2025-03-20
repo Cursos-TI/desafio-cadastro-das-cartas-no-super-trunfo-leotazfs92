d2aa0899d4ab710f90dbbb486edf27cb5995851b
Leandro Freitas Silva <leotazfs92@gmail.com>
2025-03-20 01:31:46 +0000

    Declaração de variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,7 +10,21 @@ int main() {
 
     // Mensagem de boas vindas.
     printf("Seja bem vindo, jogador!\nEspero que esteja pronto para se divertir, competir e conhecer o mundo!\n");
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
+    printf("Você vai nos ajudar a construir o jogo.\n Quero ver se você consegue pesquisar e me responder os dados de algumas cidades.")
+    
+    //Declaração das variaveis da carta 1 e 2.
+    char cod_cidade1[50], cod_cidade2;
+    char nome1[50], nome2;
+    int populacao1, populacao2;
+    float area1, area2, pib1, pib2;
+    int ponto_tur1, ponto_tur2;
+
+
+
+
+    
+
+
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
     // Cadastro das Cartas:
