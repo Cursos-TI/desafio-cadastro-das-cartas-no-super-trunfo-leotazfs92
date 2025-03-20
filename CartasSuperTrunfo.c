beebb21fb05e8c190e64c99ec1ebac09a8feefca
Leandro Freitas Silva <leotazfs92@gmail.com>
2025-03-20 02:26:09 +0000

    scanf pronto

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,54 +11,53 @@ int main() {
     char estado1[30], estado2[30], cod_cidade1[30], cod_cidade2[30], nome1[30], nome2[30];
     int populacao1, populacao2, ponto_tur1, ponto_tur2;
     float area1, area2, pib1, pib2;
-    int ponto_tur1, ponto_tur2;
 
     //Imprimindo as perguntas para o jogador e solicitando as entradas de dados
     printf("Em qual estado fica essa cidade? \n");
-    scanf(" %c", estado1);
+    scanf(" %s", estado1);
 
     printf("Qual o código da cidade? \n");
-    scanf(" %c", cod_cidade1);
+    scanf(" %s", cod_cidade1);
 
     printf("Qual o nome da cidade? \n");
-    scanf(" %c", nome1);
+    scanf(" %s", nome1);
 
     printf("Qual é a população dessa cidade? \n");
-    scanf(" %i", populacao1);
+    scanf(" %i", &populacao1);
 
     printf("Qual o tamanho de sua área em km²? \n");
-    scanf(" %f", area1);
+    scanf(" %f", &area1);
 
     printf("Quanto é o PIB? \n");
-    scanf(" %f", pib1);
+    scanf(" %f", &pib1);
 
     printf("Pra terminar, quantos pontos turísticos ela tem? \n");
-    scanf(" %i", ponto_tur1);
+    scanf(" %i", &ponto_tur1);
 
     //Nova mensagem para seguir para os dados da carta 2
     printf("Que bacana, você está se esforçando em suas pesquisas. Vamos para a próxima carta.\n");
 
     //Imprimindo as perguntas para o jogador e solicitando as entradas de dados
     printf("Em qual estado fica essa cidade? ");
-    scanf(" %c", estado2);
+    scanf(" %s", estado2);
 
     printf("Qual o código da cidade? ");
-    scanf(" %c", cod_cidade2);
+    scanf(" %s", cod_cidade2);
 
     printf("Qual o nome da cidade? ");
-    scanf(" %c", nome2);
+    scanf(" %s", nome2);
 
     printf("Qual é a população dessa cidade? ");
-    scanf(" %i", populacao2);
+    scanf(" %i", &populacao2);
 
     printf("Qual o tamanho de sua área em km²? ");
-    scanf(" %f", area2);
+    scanf(" %f", &area2);
 
     printf("Quanto é o PIB? ");
-    scanf(" %f", pib2);
+    scanf(" %f", &pib2);
 
     printf("Pra terminar, quantos pontos turísticos ela tem? ");
-    scanf(" %i", ponto_tur2);
+    scanf(" %i", &ponto_tur2);
 
     //Mensagem de interação com usuário
     printf("Excelente!! Confere aí as informações que você repassou!");
